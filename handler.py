892f6b3f8173c18a4ec809e7e9a9e1c505876297
Deepak Telkar <deepakt@ticketek.com.au>
2018-12-19 14:37:11 +1100

    handle event having multiple files

--- handler.py
+++ handler.py
@@ -1,26 +1,73 @@
+"""
+AWS Lambda to copy s3 object from source s3 bucket to Google cloud storage (similar to s3 bucket).
+google-cloud-storage package required.
+
+NOTE:
+    - Lambda's trigger should be set as source S3 bucket so, any files coming in are notified to Lambda.
+    - Environment variables:
+        LOG_LEVEL: DEBUG or INFO or ERROR
+        GCS_BUCKET_NAME: Google cloud storage bucket (destination bucket)
+        GOOGLE_APPLICATION_CREDENTIALS: File name where Google credentials are stored. This file must be
+        packaged along with the lambda package.
+"""
 import os
-import json
+import logging
 import boto3
-import urllib
+from StringIO import StringIO
 from google.cloud import storage
 
-def s3_to_gcs(event, context):
-    s3 = boto3.client("s3")
-    if event:
-        file_obj = event["Records"][0]
-        bucketname = str(file_obj['s3']['bucket']['name'])
-        print("From S3 Bucket: ", bucketname)
-        filename = str(file_obj['s3']['object']['key'])
-        filename = urllib.parse.unquote_plus(filename)
-        print("File to be copied: ", filename)
-        fileObj = s3.get_object(Bucket = bucketname, Key=filename)
-        file_content = fileObj["Body"].read().decode('utf-8')
-        
-        # Instantiates a GCP storage client
+# Setup logging
+LOG = logging.getLogger(__name__)
+LOG.setLevel(os.environ.get('LOG_LEVEL', 'INFO'))
+
+GCS_BUCKET_NAME=os.environ['GCS_BUCKET_NAME']
+S3 = boto3.client('s3')
+
+
+def lambda_handler(event, context):
+    try:
+        l_t_bucketKey = _getKeys(event)
+
+        # Create google client
         storage_client = storage.Client()
         gcs_bucket = storage_client.get_bucket(os.environ['GCS_BUCKET_NAME'])
-        print("To GCS Bucket: ", os.environ['GCS_BUCKET_NAME'])        
-        blob = gcs_bucket.blob(filename)
-        blob.upload_from_string(file_content)
-        print("Copy successful")
-    return 'Done'
+
+        LOG.debug('About to copy %d files', len(l_t_bucketKey))
+        for bucket, key in l_t_bucketKey:
+            try:
+                inFileObj = StringIO()
+                S3.download_fileobj(
+                    Bucket=bucket,
+                    Key=key,
+                    Fileobj=inFileObj
+                )
+                blob = gcs_bucket.blob(key)
+                blob.upload_from_file(inFileObj, rewind=True)  # seek(0) before reading file obj
+
+                LOG.info('Copied s3://%s/%s to gcs://%s/%s', bucket, key, GCS_BUCKET_NAME, key)
+            except:
+                LOG.exception('Error copying file: {k}'.format(k=key))
+        return 'SUCCESS'
+    except Exception as e:
+        LOG.exception("Lambda function failed:")
+        return 'ERROR'
+
+
+def _getKeys(d_event):
+    """
+    Extracts (bucket, key) from event
+
+    :param d_event: Event dict
+    :return: List of tuples (bucket, key)
+    """
+    l_t_bucketKey = []
+    if d_event:
+        if 'Records' in d_event and d_event['Records']:
+            for d_record in d_event['Records']:
+                try:
+                    bucket = d_record['s3']['bucket']['name']
+                    key = d_record['s3']['object']['key']
+                    l_t_bucketKey.append((bucket, key))
+                except:
+                    LOG.warn('Error extracting bucket and key from event')
+    return l_t_bucketKey
